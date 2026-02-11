9bca8da382118b14f420ab1378f96b85f71a26d3
lucaCarlassara <crllcu02@gmail.com>
2026-02-11 23:17:07 +0100

    freccia

--- main.js
+++ main.js
@@ -5,3 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("footer-placeholder").innerHTML = data;
     });
 });
+
+// Scroll to top button
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+    scrollBtn.style.opacity = "1";
+  } else {
+    scrollBtn.style.opacity = "0";
+    setTimeout(() => {
+      scrollBtn.style.display = "none";
+    }, 300);
+  }
+});
+
+scrollBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
